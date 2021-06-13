4c104952cebae780d8a97798287a007b506f959f
Daniel Magro <dmagro04@gmail.com>
2021-06-13 18:50:27 +0200

    Update types for PrimeNumbersService
    Updated references of Long to Int in the PrimeNumbersService and related
    classes.

--- src/main/scala/prime/numbers/PrimeNumbersServiceImpl.scala
+++ src/main/scala/prime/numbers/PrimeNumbersServiceImpl.scala
@@ -4,8 +4,8 @@ import com.prime.numbers.thrift._
 import com.twitter.inject.Logging
 import com.twitter.util.Future
 
-class PrimeNumbersServiceImpl extends PrimeNumbersService.MethodPerEndpoint  with Logging{
-  override def primeNumbersUntil(number: Long): Future[collection.Seq[Long]] = {
+class PrimeNumbersServiceImpl extends PrimeNumbersService.MethodPerEndpoint  with Logging {
+  override def primeNumbersUntil(number: Int): Future[collection.Seq[Int]] = {
     info(s"Request for: $number")
     Future.value(Seq(number))
   }
--- src/main/thrift/prime_numbers_service.thrift
+++ src/main/thrift/prime_numbers_service.thrift
@@ -1,6 +1,6 @@
 namespace scala com.prime.numbers.thrift
 
 service PrimeNumbersService {
-    list<i64> primeNumbersUntil(1: i64 number)
+    list<i32> primeNumbersUntil(1: i32 number)
 
 }
